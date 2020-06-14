b874c06b2f8ae8a5b07d34282c4f1245423cc9b5
GO <elektroceryanci@gmail.com>
2020-06-14 19:44:13 +0200

    added a new assignment of the mooc.fi course

---
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+        String a = scanner.nextLine();
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
