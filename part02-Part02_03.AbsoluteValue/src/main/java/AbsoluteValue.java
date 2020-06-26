2b38b0f816a025339ceed5404a22cec477007bf2
GO <elektroceryanci@gmail.com>
2020-06-26 23:46:30 +0200

    added a solution of part02_03

---
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = Integer.valueOf(scanner.nextLine());
+      
+        System.out.println(Math.abs(a));
+        //if(a<0){
+          //  System.out.println(-1*a); }
+        //else{
+         //   System.out.println(a);}
+
+    }
+}
