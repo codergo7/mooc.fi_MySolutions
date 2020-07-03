066ef2b0d4eccaeced75e15d4bfc73fe690f1427
GO <elektroceryanci@gmail.com>
2020-07-03 21:49:52 +0200

    added a solution of part02_16

---
+++ PART_02/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class FromWhereToWhere {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Last number?");
+        int end=Integer.valueOf(scanner.nextLine());
+        int sum=0;
+        
+        for (int  i = 1; i <= end; i++){
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
+
+        // Write your program here
+    }
+}
