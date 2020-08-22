46c20033c8375f3391b8e3b57686534d6e77c4bf
codergo7 <codergo7@gmail.com>
2020-08-22 23:36:39 +0200

    added a solution of Part04_02

---
+++ PART_04/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,14 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        Account acc = new Account("Matthews account",1000);
+        Account myAcc = new Account("My account",0);
+        acc.withdrawal(100.0);
+        myAcc.deposit(100.0);
+        System.out.println(acc);
+        System.out.println(myAcc);
+    }
+}
