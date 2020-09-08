fb679e69d3d78d593bddfb3911df4dbab0d241a3
codergo7 <codergo7@gmail.com>
2020-09-08 22:19:24 +0200

    added a solution of Part05_09

---
+++ PART_05/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -0,0 +1,23 @@
+
+public class HealthStation {
+    
+    private int weighing;
+
+    public int weigh(Person person) {
+        // return the weight of the person passed as the parameter
+        weighing++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person){
+        
+        person.setWeight(person.getWeight()+1);
+    }
+    
+    public int weighings(){
+        return weighing;
+    }
+            
+    
+
+}
