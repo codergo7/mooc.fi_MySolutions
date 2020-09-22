9e8d21ef4eb595a6d91e759cad91acdea86bcf2a
codergo7 <codergo7@gmail.com>
2020-09-22 09:01:52 +0200

    added a solution of Part08_05

---
+++ PART_08/part08-Part08_05.TodoList/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        TodoList list = new TodoList();
+ 
+        // Here you can try out the combined functionality of your classes
+        
+        Scanner scanner = new Scanner(System.in);
+ 
+        UserInterface ui = new UserInterface(list, scanner);
+        //ui.start();
+        
+        
+list.add("read the course material");
+list.add("watch the latest fool us");
+list.add("take it easy");
+list.print();
+list.remove(2);
+list.print();
+list.add("buy raisins");
+list.print();
+list.remove(1);
+list.remove(1);
+list.print();
+ 
+    }
+}
