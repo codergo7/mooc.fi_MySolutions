18152e3faf7defed888e1af7c123de34d695dc22
codergo7 <codergo7@gmail.com>
2020-09-28 17:12:35 +0200

    added a solution of Part09_09

---
+++ PART_09/part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Set;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // You can test your method here
+
+    }
+
+    public static int returnSize(Set map){
+        
+        return map.size();
+    }
+
+}
